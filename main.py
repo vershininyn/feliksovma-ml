2882aea3b5206f02f4946b796909b9bded516740
F11e22l33 <feliksovma@gmail.com>
2024-04-12 13:38:45 +0700

    Stage2

--- main.py
+++ main.py
@@ -10,14 +10,22 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
 
-os.environ['MLFLOW_TRACKING_URI'] = 'http://localhost:5000'
-os.environ['MLFLOW_S3_ENDPOINT_URL'] = 'http://localhost:9000'
+os.environ['MLFLOW_TRACKING_URI'] = 'http://127.0.0.1:5000'
+os.environ['MLFLOW_S3_ENDPOINT_URL'] = 'http://127.0.0.1:9000'
 os.environ['AWS_ACCESS_KEY_ID'] = 'minio'
 os.environ['AWS_SECRET_ACCESS_KEY'] = 'minio123'
 
+# Настройка клиента boto3
+boto3.setup_default_session(
+    aws_access_key_id='minio',
+    aws_secret_access_key='minio123',
+    region_name='us-west-1'  # или другой регион, если это применимо
+)
+
+
 # Инициализация клиента
 s3 = boto3.client('s3',
-                  endpoint_url='http://localhost:9000',
+                  endpoint_url='http://127.0.0.1:9000',
                   aws_access_key_id='minio',
                   aws_secret_access_key='minio123')
 
@@ -34,22 +42,12 @@ X_train_vec = vectorizer.fit_transform(X_train)
 X_test_vec = vectorizer.transform(X_test)
 
 # Обучение модели
-clf = LogisticRegression()
+# clf = LogisticRegression()
+clf = LogisticRegression(max_iter=150000000, solver="lbfgs")
 clf.fit(X_train_vec, y_train)
 
 # Предсказание
 y_pred = clf.predict(X_test_vec)
-
-# Настройка клиента boto3
-boto3.setup_default_session(
-    aws_access_key_id='minio',
-    aws_secret_access_key='minio123',
-    region_name='us-west-1'  # или другой регион, если это применимо
-)
-
-""" log_reg = LogisticRegression(C=args["C"], max_iter=int(args["max_iter"]),
-                                     solver="lbfgs",
-                                     class_weight={0: 0.9, 1: 0.1}) """
 
 params = {"solver": "lbfgs", "max_iter": 150000000, "model_type": "LogisticRegression"}
 
